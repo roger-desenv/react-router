82659e1b09de118dbac22080b05df8b636d3fed7
Roger Costa <roger.desenv@gmail.com>
2022-09-07 11:40:11 -0300

    adicionando rotas

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Home() {
+  return (
+    <>
+      <h1>Página Inicial</h1>
+      <ul>
+        <li>
+          <Link to="/users">Usuários</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Users.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Users() {
+  return (
+    <h1>Usuários</h1>
+  )
+}
+
+export default Users
